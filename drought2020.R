0868649669dbac2d7f912d7ca92a519463a40d23
chris <css7c3@gmail.com>
2024-07-22 23:01:48 -0600

    second commit

--- drought2020.R
+++ drought2020.R
@@ -22,3 +22,10 @@ drought2020 <- read_dta("C:/Users/css7c/OneDrive/Desktop/R_Coding/Sample_Data/da
 
 data()
 view(drought2020)
+
+# note the variables are columns, the observations are rows
+glimpse(drought2020)
+
+HouseholdInc <- filter(drought2020, R_st_name, MedianHHInc < 30000)
+NM_State <- filter(drought2020, R_st_name == "New Mexico")
+
